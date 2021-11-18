9b30319a297437053392ced42c01b2a52dd29df0
Naser Kourosh <naserkorosh1986@gmail.com>
2021-11-18 19:21:03 +0330

    add height to loadingbar

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -3,7 +3,7 @@ import LinearProgress from '@mui/material/LinearProgress';
 
 export default function LinearIndeterminate() {
   return (
-    <Box sx={{ width: '100%' }}>
+    <Box sx={{ width: '100%', height: '2rem' }}>
       <LinearProgress />
     </Box>
   );
